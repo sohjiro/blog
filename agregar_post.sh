7b2077076c5444a80aa9cbed52b4b84582d07920
carlogilmar <carlogilmar12@gmail.com>
2016-09-18 20:31:40 -0500

    Script para agregar nuevo archivo

---
+++ agregar_post.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+echo "Vamos a agregar un nuevo post, dame el nombre del fichero markdown:"
+read file
+echo "####################################################################"
+hugo new post/$file.md
+echo "Fichero creado, búscalo en /content/post/"
+echo "Para correr el servidor local con cambios, ejecuta:"
+echo "                                                   hugo serve --buildDrafs"
+
