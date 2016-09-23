1f4eb8511508ac734351dc72c0e23d7f99326316
carlogilmar <carlogilmar12@gmail.com>
2016-09-23 10:52:38 -0500

    Script para actualizar

---
+++ update.sh
@@ -0,0 +1,7 @@
+echo "--------------------Actualizando--------------------------"
+git pull origin master
+echo "-----------------------------------------------Repositorio Blog ok"
+cd public
+git pull origin master
+echo "...............................................Repositorio MakingDevs.Gitub.Io en Public ok"
+echo "Sitios actualizados"
